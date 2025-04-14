7146aac7df09e347fe69e77b5d419c9f2f77de29
Arthur Ritzel <arthur.ritzel@costaoeste.srv>
2025-04-14 14:40:45 -0300

    oder and debt

--- src/consumer.js
+++ src/consumer.js
@@ -4,16 +4,57 @@ import connection from './services/connection.js';
 
 dotenv.config();
 
-const queue = 'processTask';
-const exchange = 'processTaskExchange';
-const routingKey = 'task';
+const taskQueue = 'taskQueue'; 
+const debtQueue = 'debtQueue'; 
+const orderQueue = 'orderQueue';
+const exchange = 'processExchange';
 
-connection(queue, exchange, routingKey, (task) => {
-    
+const processTask = (task) => {
     fetch(task.data.callback.href, {
-        method: task.data.callback.method,
-        headers: {
-            "x-api-key": task.data.callback.token,
-        }
-    })
-})
+      method: task.data.callback.method,
+      headers: {
+        "x-api-key": task.data.callback.token,
+      },
+    });
+
+};
+
+const processDebt =  (order) => {
+    const { userId, products, orderId } = order.data;
+
+    const subtotal = products.reduce(
+      (acc, product) => acc + product.quantity * product.value, 0
+    );
+
+    fetch(order.data.callback.href, {
+      method: order.data.callback.method,
+      headers: {
+        "x-api-key": order.data.callback.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: userId,
+        order: orderId,
+        products,
+        totalAmount: subtotal,
+      }),
+    });
+};
+
+const processOrder = (order) => {
+    const { status } = order.data;
+
+    fetch(order.data.callback.href, {
+      method: order.data.callback.method,
+      headers: {
+        "x-api-key": order.data.callback.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ status }),
+    });
+};
+
+// Conectando e consumindo as filas
+connection(taskQueue, exchange, taskQueue, processTask);
+connection(debtQueue, exchange, debtQueue, processDebt);
+connection(orderQueue, exchange, orderQueue, processOrder);
